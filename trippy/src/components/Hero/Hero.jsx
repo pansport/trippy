17e0b609d5277333d8533f7ee3d76355b7af342e
Kristijan Horvatić <111249835+pansport@users.noreply.github.com>
2023-04-26 12:05:30 +0200

    Add files via upload

---
+++ trippy/src/components/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Hero.css";
+
+export default function Hero(props) {
+  return (
+    <div className={props.cName}>
+      <img src={props.image} alt="hero image" />
+      <div className="hero-text">
+        <h1>{props.title}</h1>
+        <p>{props.text}</p>
+        <a href={props.url} className={props.btnClass}>
+          {props.btnText}
+        </a>
+      </div>
+    </div>
+  );
+}
